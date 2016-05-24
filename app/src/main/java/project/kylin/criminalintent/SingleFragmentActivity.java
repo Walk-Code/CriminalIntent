bfc8e3fce9d9db58c404fb5a618e89fdf0a8ca53
Walk_Code <walk_code@163.com>
2016-05-24 22:30:36 +0800

    Use Simple ToolBar

--- app/src/main/java/project/kylin/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/project/kylin/criminalintent/SingleFragmentActivity.java
@@ -6,17 +6,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by jianqi on 2016/4/30.
  */
 public abstract class SingleFragmentActivity extends AppCompatActivity{
     protected abstract Fragment createFragment();
+    private Toolbar mToolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+        //set toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
         if (null == fragment) {
