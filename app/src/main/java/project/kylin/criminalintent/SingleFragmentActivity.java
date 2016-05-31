c25f9dde049a362642315d23120540fad4585a17
Walk_Code <walk_code@163.com>
2016-05-31 23:02:50 +0800

    ToolBar点击事件存在BUG

--- app/src/main/java/project/kylin/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/project/kylin/criminalintent/SingleFragmentActivity.java
@@ -7,6 +7,11 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Toast;
 
 /**
  * Created by jianqi on 2016/4/30.
@@ -17,11 +22,35 @@ public abstract class SingleFragmentActivity extends AppCompatActivity{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
         //set toolbar
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.mipmap.ic_menu);
+        //mToolbar.setLogo(R.mipmap.ic_launcher);
+        mToolbar.setTitle(R.string.crime_title);
+        mToolbar.setTitleTextColor(0xffffffff);
+        mToolbar.inflateMenu(R.menu.base_toolbar_menu);//set menu
+
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.action_add:
+                         Toast.makeText(SingleFragmentActivity.this,"click",Toast.LENGTH_SHORT).show();
+
+                    case R.id.action_notification:
+                        Toast.makeText(SingleFragmentActivity.this,"click notification",Toast.LENGTH_SHORT).show();
+
+                    case R.id.action_search:
+                        Toast.makeText(SingleFragmentActivity.this,"click search",Toast.LENGTH_SHORT).show();
+                }
+
+                return true;
+            }
+        });
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
